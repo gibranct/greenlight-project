c7da70fb3dc13423b4d5ac65fe21d243711edc7f
gibran.tavares <gibran.tavares@arcotech.io>
2024-06-02 13:43:07 -0300

    first commit

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	data := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
+
+	err := app.writeJson(w, http.StatusOK, data, nil)
+
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(
+			w,
+			"The server encountered a problem and could not process your request",
+			http.StatusInternalServerError,
+		)
+		return
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com.br/gibranct/greenlight
+
+go 1.22.1
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
